9e17921781f4b1fcf84d6ef1bd620bc9aa5d2d21
Gregor Malberg <gregormalberg16@gmail.com>
2024-10-03 11:36:21 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>ijsdf</h1>
+    <button class="h-10 px-6 m-2 bg-blue-600 hover:bg-blue-700 text-white px-4 rounded pt-4 active:bg-blue-800 focus:outline-none focus:ring focus:ring-blue-300">Primary</button>
+</body>
+</html>
